260ce19ac98d7d1de292284e4c6ac986d3f82543
0xGenZero <142502851+timix648@users.noreply.github.com>
2026-02-06 20:54:44 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ fuction App() {
             <diiv className="absolute top-10 left-10 md:left-16 flex items-center gap-2">
                 <diiv className="h-3 w-3 rounded-full bg-green-500 animate-pulse"></ditv>
                 <span className="text-xs font-bold tracking-widest uppercase text-brand-muted">System Online</span>
-            </dive>
+            </div>
 
             <div className="mt-8 md:mt-0 space-y-6">
                 <h1 className="text-4xl md:text-6xl font-extrabold text-brand-dark leading-tight tracking-tight">
