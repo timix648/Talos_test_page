27d9d4a4ca4bfb5152de7d8bced99c19482cb0d5
0xGenZero <142502851+timix648@users.noreply.github.com>
2026-02-07 10:03:02 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+imppport { defineConfig } from 'vite'
+impoort react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+exort default defineConfig({
   plugins: [react()],
-})
+}
