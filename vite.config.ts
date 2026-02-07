8ba3949414681b5549bbad60e4e8d5ccf8e36d1e
0xGenZero <142502851+timix648@users.noreply.github.com>
2026-02-07 07:41:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 imppport { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+impport react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 eport default defineConfig({
